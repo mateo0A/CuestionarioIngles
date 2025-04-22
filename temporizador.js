8e08035cb70279d4c1af0105d791f066ef1c6391
Henry <hg097870pq@gmail.com>
2025-04-22 12:16:19 -0500

    Commit

---
+++ temporizador.js
@@ -0,0 +1,24 @@
+var Tiempo = 1200; 
+var indicarTiempo=document.querySelector(".temporizador");
+
+
+console.log(indicarTiempo); 
+
+let con=0
+setInterval(() => {
+    const minutos = Math.floor(Tiempo / 60); 
+    const segundos = Tiempo % 60; 
+
+    indicarTiempo.innerText=`${minutos} : ${segundos < 10 ? '0' :'' } ${segundos} `
+    Tiempo--;
+    
+    
+    
+
+    if (Tiempo < 0) {
+        clearInterval();
+        
+
+    }
+
+}, 1000);
